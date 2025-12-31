bd7625d410a32aa29c7beea9ee49877a65027b83
Sepehr Asgarian <sepehrasgarian21@gmail.com>
2025-12-31 18:14:05 +0330

    hard reset

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Auth & Messaging System")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # بعداً محدودش کن
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],  # GET, POST, OPTIONS, ...
     allow_headers=["*"],
